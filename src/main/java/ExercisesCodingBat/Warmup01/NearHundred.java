80f727edc0ea3952e0741742f1cc654c7a51d36c
Lucas Silva <lucas.silva.s1998@gmail.com>
2026-05-28 14:20:37 -0300

    Update NearHundred.java

--- src/main/java/ExercisesCodingBat/Warmup01/NearHundred.java
+++ src/main/java/ExercisesCodingBat/Warmup01/NearHundred.java
@@ -1,4 +1,21 @@
-package ExercisesCodingBat.Warmup01;
+/*
+Given an int n, return true if it is within 10 of 100 or 200.
+Note: Math.abs(num) computes the absolute value of a number.
 
-public class NearHundred {
+nearHundred(93) → true
+nearHundred(90) → true
+nearHundred(89) → false*/
+
+public boolean nearHundred(int n) {
+    boolean result = false;
+
+    if(Math.abs(100 - n) <= 10 ) {
+        result = true;
+    }
+    return result;
+}
+
+
+void main(String[] args) {
+
 }
