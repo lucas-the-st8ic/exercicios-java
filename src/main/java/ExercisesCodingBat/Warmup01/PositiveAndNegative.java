2caeca371fcfeafc2af6dea951474206b0d75c8a
Lucas Silva <lucas.silva.s1998@gmail.com>
2026-05-28 16:40:31 -0300

    Create PositiveAndNegative.java

---
+++ src/main/java/ExercisesCodingBat/Warmup01/PositiveAndNegative.java
@@ -0,0 +1,36 @@
+/*
+Given 2 int values, return true if one is negative
+and one is positive.
+Except if the parameter "negative" is true,
+ then return true only if both are negative.
+
+posNeg(1, -1, false) → true
+posNeg(-1, 1, false) → true
+posNeg(-4, -5, true) → true
+
+Dados dois valores inteiros,
+retorne verdadeiro se um for negativo e o outro for positivo.
+Exceto se o parâmetro "negativo" for verdadeiro,
+nesse caso, retorne verdadeiro somente se ambos forem negativos.
+
+*/
+
+public boolean posNeg(int a, int b, boolean negative) {
+    boolean result = false;
+
+    if (negative && (a < 0 && b < 0)) {
+        result = true;
+    } else if (!negative && (a < 0 && b > 0) ||
+            !negative && (a > 0 && b < 0) ) {
+        result = true;
+    }
+    System.out.println(result);
+    return result;
+
+}
+
+
+
+void main(String[] args) {
+    posNeg(1, -1, true);
+}
