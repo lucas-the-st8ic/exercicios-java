2db786bb2eb12633aeb7fda1f82225404e4401e7
Lucas Silva <lucas.silva.s1998@gmail.com>
2026-05-31 20:01:44 -0300

    Update LFront3.java

--- src/main/java/ExercisesCodingBat/Warmup01/LFront3.java
+++ src/main/java/ExercisesCodingBat/Warmup01/LFront3.java
@@ -1,5 +1,21 @@
 
 
+/*
+Given a string, we'll say that the front is the first 3
+chars of the string. If the string length is less than 3,
+the front is whatever is there. Return a new string which
+is 3 copies of the front.
+
+Dada uma string, diremos que a frente são os 3 primeiros
+caracteres da string. Se o comprimento da string for menor
+que 3, a frente será o que estiver presente. Retorne uma
+nova string que seja 3 cópias da frente.
+
+front3("Java") → "JavJavJav"
+front3("Chocolate") → "ChoChoCho"
+front3("abc") → "abcabcabc"
+*/
+
 
 void main(String[] args) {
     front3("Chocolate");
@@ -18,3 +34,21 @@ public String front3(String string) {
     System.out.println(newString);
     return newString;
 }
+
+/*
+CodingBat solution:
+    public String front3(String str) {
+      String front;
+
+      if (str.length() >= 3) {
+        front = str.substring(0, 3);
+      }
+      else {
+        front = str;
+      }
+
+      return front + front + front;
+    }
+
+
+*/
