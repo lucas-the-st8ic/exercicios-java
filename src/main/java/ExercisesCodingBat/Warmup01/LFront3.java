6db4609dc8a5ba3ec3f13bd2460875752117f4fe
Lucas Silva <lucas.silva.s1998@gmail.com>
2026-05-31 19:51:47 -0300

    Update LFront3.java

--- src/main/java/ExercisesCodingBat/Warmup01/LFront3.java
+++ src/main/java/ExercisesCodingBat/Warmup01/LFront3.java
@@ -2,5 +2,19 @@
 
 
 void main(String[] args) {
+    front3("Chocolate");
+    front3("Java");
+    front3("Cd");
+}
 
+public String front3(String string) {
+    if (string.length() <= 3) {
+        System.out.println(string.concat(string+string));
+        return string.concat(string+string);
+    }
+
+    String first3Chars = string.substring(0,3);
+    String newString = first3Chars.concat(first3Chars + first3Chars);
+    System.out.println(newString);
+    return newString;
 }
