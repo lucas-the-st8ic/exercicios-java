b86de2b2c5b5d4e976d51fb8ccb84ab482a0bda2
Lucas Silva <lucas.silva.s1998@gmail.com>
2026-06-01 17:29:57 -0300

    Update Or35.java

--- src/main/java/ExercisesCodingBat/Warmup01/Or35.java
+++ src/main/java/ExercisesCodingBat/Warmup01/Or35.java
@@ -1,3 +1,18 @@
+/*
+Return true if the given non-negative number is a multiple of 3 or a
+multiple of 5. Use the % "mod" operator -- see Introduction to Mod
+
+Retorna verdadeiro se o número não negativo fornecido for múltiplo de 3
+ou múltiplo de 5. Use o operador % "mod" -- veja Introdução ao operador
+Mod.
+
+
+or35(3) → true
+or35(10) → true
+or35(8) → false
+*/
+
+
 void main(String[] args) {
     or35(3);
     or35(10);
