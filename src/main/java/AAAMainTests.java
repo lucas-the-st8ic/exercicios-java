8b26860ec5664cb3d2cca9241698a289e1be0aab
Lucas Silva <lucas.silva.s1998@gmail.com>
2026-06-06 19:27:35 -0300

    updates

--- src/main/java/AAAMainTests.java
+++ src/main/java/AAAMainTests.java
@@ -12,5 +12,18 @@ public class AAAMainTests {
 
         String substring = string.substring(0, n + 1);
         System.out.println(substring);
+        in1020(8, 25);
+        in1020(21, 12);
+        in1020(12, 99);
+        in1020(8, 99);
     }
+
+    static boolean result;
+    public static void in1020(int num_1, int num_2) {
+        result = (num_1 >= 10 && num_1 <= 20) ||
+                (num_2 >= 10 && num_2 <= 20);
+        System.out.println(result);
+    }
+
+
 }
---
+++ src/main/java/ExercisesCodingBat/Warmup01/RIn1020.java
@@ -0,0 +1,13 @@
+
+
+void main(String[] args) {
+    in1020(8, 21);
+}
+
+public boolean in1020(int num_1, int num_2) {
+    if((num_1 >= 10 && num_1 <= 20) ||
+            (num_2 >=10 && num_2 <= 20)) {
+        return true;
+    }
+    return false;
+}
