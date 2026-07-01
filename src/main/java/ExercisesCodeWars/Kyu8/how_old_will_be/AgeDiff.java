0aee903441fa10bbbb9d64ea00eb7492b7f60313
Lucas Silva <lucas.silva.s1998@gmail.com>
2026-07-01 11:53:52 -0300

    Create AgeDiff.java

---
+++ src/main/java/ExercisesCodeWars/Kyu8/how_old_will_be/AgeDiff.java
@@ -0,0 +1,28 @@
+package ExercisesCodeWars.Kyu8.how_old_will_be;
+
+public class AgeDiff {
+    public static String CalculateAge(int birth, int yearTo) {
+        int difference = Math.abs(birth - yearTo);
+        String wordYear = difference == 1 ? " year" : " years";
+        if(birth == yearTo) {
+            return "You were born this very year!";
+        } else if(birth > yearTo) {
+            return "You will be born in " +difference+ wordYear+ ".";
+        } else {
+            return "You are " +difference+ wordYear+ " old.";
+        }
+    }
+
+/* Outra solução com menos linhas de código:
+
+public class AgeDiff {
+  public static String CalculateAge(int birth, int year) {
+    final int age = year - birth;
+    return
+      age == 0 ? "You were born this very year!" :
+      age > 0 ? String.format("You are %d year%s old.", age, age == 1 ? "" : "s") :
+      String.format("You will be born in %d year%s.", -age, -age == 1 ? "" : "s");
+  }
+}
+*/
+}
