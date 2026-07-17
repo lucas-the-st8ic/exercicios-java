8243e839acb5faefb72aea1f4761d8a1aeebf4bb
Lucas Silva <lucas.silva.s1998@gmail.com>
2026-07-17 14:36:07 -0300

    Update MainTests.java

--- src/main/java/MainTests.java
+++ src/main/java/MainTests.java
@@ -70,20 +70,11 @@ public class MainTests {
         for (int i = 0; i < string.length(); i++) {
             switch (string.charAt(i)) {
                 case 'a':
-                    count++;
-                    break;
                 case 'e':
-                    count++;
-                    break;
                 case 'i':
-                    count++;
-                    break;
                 case 'o':
-                    count++;
-                    break;
                 case 'u':
                     count++;
-                    break;
                 default:
                     break;
             }
