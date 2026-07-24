e9d9a9b68b00fc70160abf72ae7df897813b1104
Lucas Silva <lucas.silva.s1998@gmail.com>
2026-07-24 14:32:38 -0300

    Update Kata.java

--- src/main/java/ExercisesCodeWars/Kyu8/convert_number_to_string/Kata.java
+++ src/main/java/ExercisesCodeWars/Kyu8/convert_number_to_string/Kata.java
@@ -1,4 +1,8 @@
 package ExercisesCodeWars.Kyu8.convert_number_to_string;
 
 public class Kata {
+    public static String numberToString(int num) {
+        String numToString = String.valueOf(num);
+        return numToString;// Return a string of the number here!
+    }
 }
