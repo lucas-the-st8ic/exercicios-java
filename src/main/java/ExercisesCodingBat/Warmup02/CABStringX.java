f2ff698e5aa7a073a6bb1d1221478b6976b36e2b
Lucas Silva <lucas.silva.s1998@gmail.com>
2026-07-26 21:06:43 -0300

    Update CABStringX.java

--- src/main/java/ExercisesCodingBat/Warmup02/CABStringX.java
+++ src/main/java/ExercisesCodingBat/Warmup02/CABStringX.java
@@ -1,3 +1,19 @@
+/*Given a string, return a version where
+all the "x" have been removed.
+Except an "x" at the very start or
+end should not be removed.
+
+Dada uma string, retorne uma versão na qual todos os
+caracteres "x" tenham sido removidos.
+No entanto, um "x" situado exatamente no início ou
+no final não deve ser removido.
+
+stringX("xxHxix") → "xHix"
+stringX("abxxxcd") → "abcd"
+stringX("xabxxxcdx") → "xabcdx"
+
+*/
+
 void main(String[] args) {
     stringX("xxHxix"); //→ "xHix"
     stringX("abxxxcd"); //→ "abcd"
@@ -18,3 +34,15 @@ public String stringX (String string) {
     System.out.println(result);
     return result;
 }
+/*
+CodingBat Solution:
+    public String stringX(String str) {
+      String result = "";
+      for (int i=0; i<str.length(); i++) {
+        // Only append the char if it is not the "x" case
+        if (!(i > 0 && i < (str.length()-1) && str.substring(i, i+1).equals("x"))) {
+          result = result + str.substring(i, i+1); // Could use str.charAt(i) here
+        }
+      }
+      return result;
+    }                   */
