8d0c4ec6cfc5ec4fe5ac73fb232228984c8160e0
Lucas Silva <lucas.silva.s1998@gmail.com>
2026-07-27 20:01:10 -0300

    Update CBBAltPairs.java

--- src/main/java/ExercisesCodingBat/Warmup02/CBBAltPairs.java
+++ src/main/java/ExercisesCodingBat/Warmup02/CBBAltPairs.java
@@ -1,13 +1,13 @@
 /*
 
-*/
+ */
 void main(String[] args) {
-altPairs("kitten"); //→ "kien"
-altPairs("Chocolate"); //→ "Chole"
-altPairs("CodingHorror"); //→ "Congrr"
-altPairs2("kitten"); //→ "kien"
-altPairs2("Chocolate"); //→ "Chole"
-altPairs2("CodingHorror"); //→ "Congrr"
+    altPairs("kitten"); //→ "kien"
+    altPairs("Chocolate"); //→ "Chole"
+    altPairs("CodingHorror"); //→ "Congrr"
+    altPairs2("kitten"); //→ "kien"
+    altPairs2("Chocolate"); //→ "Chole"
+    altPairs2("CodingHorror"); //→ "Congrr"
 }
 
 public String altPairs(String string) {
@@ -21,27 +21,28 @@ public String altPairs(String string) {
     System.out.printf(newString);
     return newString;
 }
+
 /*CodingBat Solution:
 Pega dois caractéres por vez com o substring
-    public String altPairs(String str) {
-      String result = "";
+public String altPairs(String str) {
+  String result = "";
 
-      // Run i by 4 to hit 0, 4, 8, ...
-      for (int i=0; i<str.length(); i += 4) {
-        // Append the chars between i and i+2
-        int end = i + 2;
-        if (end > str.length()) {
-          end = str.length();
-        }
-        result = result + str.substring(i, end);
-      }
+  // Run i by 4 to hit 0, 4, 8, ...
+  for (int i=0; i<str.length(); i += 4) {
+    // Append the chars between i and i+2
+    int end = i + 2;
+    if (end > str.length()) {
+      end = str.length();
+    }
+    result = result + str.substring(i, end);
+  }
 
-      return result;
+  return result;
 }*/
 public String altPairs2(String string) {
 
     String newString = "";
-    for (int i = 0; i < string.length();) {
+    for (int i = 0; i < string.length(); ) {
         newString += string.charAt(i);
         i += (i % 2 == 0) ? 1 : 3;
     }
