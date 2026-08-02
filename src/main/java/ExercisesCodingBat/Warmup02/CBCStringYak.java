6337bdb566adfb275f002db0a711c6a4b90287d8
Lucas Silva <lucas.silva.s1998@gmail.com>
2026-08-02 19:00:12 -0300

    Update CBCStringYak.java

--- src/main/java/ExercisesCodingBat/Warmup02/CBCStringYak.java
+++ src/main/java/ExercisesCodingBat/Warmup02/CBCStringYak.java
@@ -7,4 +7,24 @@ void main(String[] args) {
 public String stringYak(String string) {
     String result = "";
 
+    System.out.println(string.length() - 2);
+    for (int i = 0; i < string.length() - 2; i++) {
+        System.out.println("Índice: " + i);
+        System.out.println("================");
+        System.out.println(string.length() - 2);
+        if (string.charAt(i) != 'y' && string.charAt(i + 2) != 'z') {
+            System.out.println("String do índice i: " + string.charAt(i));
+            System.out.println("String do índice i + 2: " + string.charAt(i + 2));
+            System.out.println("================");
+            result += string.substring(i, i + 3);
+            System.out.println(result);
+            System.out.println("================");
+            if (i + 2 < string.length()) {
+                i += 2;
+            }
+        }
+    }
+
+    System.out.println(result);
+    return result;
 }
