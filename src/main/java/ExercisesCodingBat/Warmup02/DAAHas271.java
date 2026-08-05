a8fb7566d33c081abe18af2874cccf368f4bb21d
Lucas Silva <lucas.silva.s1998@gmail.com>
2026-08-05 15:53:26 -0300

    Update DAAHas271.java

--- src/main/java/ExercisesCodingBat/Warmup02/DAAHas271.java
+++ src/main/java/ExercisesCodingBat/Warmup02/DAAHas271.java
@@ -1,3 +1,15 @@
+/*Given an array of ints, return true if it
+contains a 2, 7, 1 pattern: a value,
+followed by the value plus 5,
+followed by the value minus 1.
+Additionally the 271 counts even if the "1"
+differs by 2 or less from the correct value.
+
+has271([1, 2, 7, 1]) → true
+has271([1, 2, 8, 1]) → false
+has271([2, 7, 1]) → true
+*/
+
 void main(String[] args) {
     has271(new int[]{1, 2, 7, 1});  // → true
     has271(new int[]{1, 2, 8, 1});  // → false
@@ -7,7 +19,7 @@ void main(String[] args) {
 public boolean has271(int[] numbers) {
     for (int i = 0; i < numbers.length - 2; i++) {
         if (numbers[i + 1] == numbers[i] + 5
-        && Math.abs(numbers[i + 2] - numbers[i - 1]) <= 2) {
+        && Math.abs(numbers[i + 2] - (numbers[i] - 1)) <= 2) {
             return true;
         }
     }
