32935ce78b32bfcfa9cbb351a010a2c53be12094
Lucas Silva <lucas.silva.s1998@gmail.com>
2026-08-05 15:37:49 -0300

    Create DAAHas271.java

---
+++ src/main/java/ExercisesCodingBat/Warmup02/DAAHas271.java
@@ -0,0 +1,18 @@
+void main(String[] args) {
+    has271(new int[]{1, 2, 7, 1});  // → true
+    has271(new int[]{1, 2, 8, 1});  // → false
+    has271(new int[]{2, 7, 1});  //→ true
+}
+
+public boolean has271(int[] numbers) {
+    for (int i = 0; i < numbers.length; i++) {
+        return numbers[i + 1] == numbers[i] + 5
+                && Math.abs(numbers[i + 2] - numbers[i - 1]) <= 2;
+    }
+    return false;
+}
+/*
+X
+X + 5
+X - 1
+*/
