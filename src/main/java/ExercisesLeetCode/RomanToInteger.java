de9b54a56ff46d13208fa46d6f95dd70a0587160
Lucas Silva <lucas.silva.s1998@gmail.com>
2026-08-09 17:11:33 -0300

    Update RomanToInteger.java

--- src/main/java/ExercisesLeetCode/RomanToInteger.java
+++ src/main/java/ExercisesLeetCode/RomanToInteger.java
@@ -1,21 +1,32 @@
 package ExercisesLeetCode;
 
-import java.util.HashMap;
 import java.util.Map;
 
 public class RomanToInteger {
-    private static final Map<String, Integer> NUMERAIS_ROMANOS = Map.of(
-            "I", 1,
-            "V", 5,
-            "X", 10,
-            "L", 50,
-            "C", 100,
-            "D", 500,
-            "M", 1000);
+    private static final Map<Character, Integer> NUMERAIS_ROMANOS = Map.of(
+            'I', 1,
+            'V', 5,
+            'X', 10,
+            'L', 50,
+            'C', 100,
+            'D', 500,
+            'M', 1000);
 
     public int romanToInt(String s) {
-        
-        System.out.println(s);
-        return 0;
+        int result = 0;
+        int previousValue = 0;
+
+        for (int i = s.length() - 1; i >= 0; i --) {
+            char currentChar = s.charAt(i);
+            int value = NUMERAIS_ROMANOS.get(currentChar);
+
+            if (value < previousValue) {
+                result -= value;
+            } else {
+                result += value;
+            }
+            previousValue = value;
+        }
+        return result;
     }
 }
