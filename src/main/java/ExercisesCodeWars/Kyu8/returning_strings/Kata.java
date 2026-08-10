e36814093df83181567bcd144a0d4afcf770e090
Lucas Silva <lucas.silva.s1998@gmail.com>
2026-08-10 14:09:56 -0300

    Create Kata.java

---
+++ src/main/java/ExercisesCodeWars/Kyu8/returning_strings/Kata.java
@@ -0,0 +1,7 @@
+package ExercisesCodeWars.Kyu8.returning_strings;
+
+public class Kata {
+    public static String greet(String name) {
+        return "Hello, " + name + " how are you doing today?";
+    }
+}
