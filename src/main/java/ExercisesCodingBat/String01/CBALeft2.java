86c8ebf64a3b12310809f842b59fe152624e3016
Lucas Silva <lucas.silva.s1998@gmail.com>
2026-08-30 10:39:10 -0300

    Update CBALeft2.java

--- src/main/java/ExercisesCodingBat/String01/CBALeft2.java
+++ src/main/java/ExercisesCodingBat/String01/CBALeft2.java
@@ -1,12 +1,15 @@
 void main() {
-
+    left2("Hello"); //→ "lloHe"
+    left2("java"); //→ "vaja"
+    left2("Hi"); //→ "Hi"
 }
 
 
 public String left2(String string) {
 
+    String result = string.substring(2)
+            .concat(string.substring(0,2));
 
-    String result = string.substring(2).concat(string.substring(0,2));
     return result;
 }
 
